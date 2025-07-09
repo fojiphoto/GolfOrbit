6de2bb309a099b3b2eeea6c0ed7a58dcfa4d8137
Nawaz Ali <fojiphoto161@gmail.com>
2025-07-09 14:25:59 +0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'unity-game-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/Build/yourbuild.wasm',
+    '/Build/yourbuild.data',
+    '/Build/yourbuild.framework.js',
+    '/Build/yourbuild.loader.js',
+    '/TemplateData/style.css',
+    '/icon-192.png',
+    '/icon-512.png'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
