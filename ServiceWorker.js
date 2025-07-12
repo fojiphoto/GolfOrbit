585fd70786d0335f6bdb078de728b660e1c215ef
Nawaz Ali <fojiphoto161@gmail.com>
2025-07-12 16:43:07 +0500

    Build Update

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,8 +2,8 @@ const cacheName = "TN-Golf Orbit-1.25.39";
 const contentToCache = [
     "Build/9853637125e801e9aae48e78dbbdcfca.loader.js",
     "Build/bdf9373a9ebf9452b80abcf61f567afa.framework.js.unityweb",
-    "Build/b43d2a6a95fa276c6846aa2a20f3e215.data.unityweb",
-    "Build/6c3ff0333cb8ef01ed43408bec8e9497.wasm.unityweb",
+    "Build/799b1880e2eaf435504457c40fc8bc9f.data.unityweb",
+    "Build/f7e9b7135182349b2dd6ead7eed17a68.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
