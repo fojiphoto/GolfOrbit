d384ad24de55e40f62c92ed4fa8e8f38256a1635
Nawaz Ali <fojiphoto161@gmail.com>
2025-07-14 14:46:01 +0500

    Add files via upload

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,7 +2,7 @@ const cacheName = "PinpinTeam-Golf Orbit-1.25.39";
 const contentToCache = [
     "Build/9853637125e801e9aae48e78dbbdcfca.loader.js",
     "Build/c5e3ce1a0242c704135be4d866484562.framework.js.unityweb",
-    "Build/d74ce367a26fe495c33122534d6a3621.data.unityweb",
+    "Build/453f9fec6de1898bfda92f3edc4b9fb5.data.unityweb",
     "Build/f4ecbadad57c4ebc88341fe9350e277e.wasm.unityweb",
     "TemplateData/style.css"
 
--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@ const CACHE_NAME = 'Golf-Orbit-v1';
 const urlsToCache = [
     '/',
     '/index.html',
-    '/Build/d74ce367a26fe495c33122534d6a3621.data.unityweb',
+    '/Build/453f9fec6de1898bfda92f3edc4b9fb5.data.unityweb',
     '/Build/f4ecbadad57c4ebc88341fe9350e277e.wasm.unityweb',
     '/Build/c5e3ce1a0242c704135be4d866484562.framework.js.unityweb',
     '/Build/9853637125e801e9aae48e78dbbdcfca.loader.js',
